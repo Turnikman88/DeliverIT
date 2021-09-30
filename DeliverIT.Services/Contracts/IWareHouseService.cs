f0c54a0c9043219d0791efee7fe1957c4bc3d330
Georgi Petrov <georgi.petrov1820@abv.bg>
2021-09-30 12:10:20 +0300

    Add WareHouse service, api, DTO

--- DeliverIT.Services/Contracts/IWareHouseService.cs
+++ DeliverIT.Services/Contracts/IWareHouseService.cs
@@ -1,11 +1,14 @@
 ﻿using DeliverIT.Models.DatabaseModels;
 using DeliverIT.Services.DTOs;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DeliverIT.Services.Contracts
 {
-    public interface IWareHouseService
+    public interface IWareHouseService : ICRUDshared<WareHouseDTO>
     {
-        IEnumerable<WareHouseDTO> Locations();
+        Task<bool> WareHouseExists(int id);
+        //WareHouseDTO Updatea(int id, WareHouseDTO obj);
+
     }
 }
--- DeliverIT.Services/Services/WareHouseService.cs
+++ DeliverIT.Services/Services/WareHouseService.cs
@@ -1,10 +1,12 @@
 ﻿using DeliverIT.Models;
+using DeliverIT.Models.DatabaseModels;
 using DeliverIT.Services.Contracts;
 using DeliverIT.Services.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading.Tasks;
+using DeliverIT.Services.DTOMappers;
 namespace DeliverIT.Services.Services
 {
     public class WareHouseService : IWareHouseService
@@ -15,17 +17,96 @@ namespace DeliverIT.Services.Services
         {
             this.db = db;
         }
-        public IEnumerable<WareHouseDTO> Locations()
+
+        public async Task<WareHouseDTO> Delete(int id)
         {
-            var WareHouseDTO = db.WareHouses
-                .Include(x => x.Address)
+            var model = await this.db.WareHouses.FindAsync(id);
+            var modelGTO = model.GetDTO();
+            this.db.WareHouses.Remove(model);
+            await db.SaveChangesAsync();
+
+            return modelGTO;
+        }
+
+        public async Task<IEnumerable<WareHouseDTO>> Get()
+        {
+            var WareHousesDTO = await db.WareHouses
                 .Select(x => new WareHouseDTO
-                {
+                { 
                     Id = x.Id,
-                    StreetName = x.Address.StreetName
+                    StreetName = x.Address.StreetName,
+                    City = x.Address.City.Name,
+                    Country = x.Address.City.Country.Name
                 })
-                .ToList();
-            return WareHouseDTO;
+                .ToListAsync();
+
+            return WareHousesDTO;
+        }
+
+        public async Task<WareHouseDTO> Post(WareHouseDTO obj)
+        {
+            int addressId = await GetAddresId(obj);
+            var model = new WareHouse
+            {
+                AddressId = addressId
+            };
+            
+            var newWareHouse = await db.WareHouses.AddAsync(model);
+            await db.SaveChangesAsync();
+            obj.Id = await db.WareHouses.Where(x => x.AddressId == addressId).Select(x => x.Id).FirstOrDefaultAsync();
+            return obj;
+        }
+
+        public async Task<WareHouseDTO> Update(int id, WareHouseDTO obj) 
+        {
+            var model = await this.db.WareHouses.FindAsync(id);
+            int addressId = await GetAddresId(obj);
+            model.AddressId = addressId;
+            obj.Id = model.Id;
+            await db.SaveChangesAsync();
+            return obj;
+        }
+
+        public async Task<bool> WareHouseExists(int id) 
+        {
+            var model = await db.WareHouses.FirstOrDefaultAsync(x => x.Id == id);
+            return model is null ? false : true;
+        }
+        private async Task<int> GetAddresId(WareHouseDTO obj)
+        {
+            var address = await db.Addresses.FirstOrDefaultAsync(x => x.StreetName == obj.StreetName);
+            var city = await db.Cities.FirstOrDefaultAsync(x => x.Name == obj.City);
+            var country = await db.Countries.FirstOrDefaultAsync(x => x.Name == obj.Country);
+
+            if (country is null)
+            {
+                await this.db.Countries.AddAsync(new Country { Name = obj.Country });
+                await db.SaveChangesAsync();
+
+            }
+            if (city is null)
+            {
+                await this.db.Cities.AddAsync(new City
+                {
+                    CountryId = await db.Countries.Where(x => x.Name == obj.Country).Select(x => x.Id).FirstOrDefaultAsync(),
+                    Name = obj.City
+                });
+                await db.SaveChangesAsync();
+
+            }
+
+            if (address is null)
+            {
+                address = new Address
+                {
+                    CityId = await db.Cities.Where(x => x.Name == obj.City).Select(x => x.Id).FirstOrDefaultAsync(),
+                    StreetName = obj.StreetName
+                };
+                await this.db.Addresses.AddAsync(address);
+                await db.SaveChangesAsync();
+            }
+
+            return address.Id;
         }
     }
 }
