d91d670685bcf08267ffd95bed3664309d0018fe
Kalin Balimezov <kbalimezov@gmail.com>
2021-10-01 11:30:42 +0300

    Seeder updated

--- DeliverIT.Models/DAL/Seeder.cs
+++ DeliverIT.Models/DAL/Seeder.cs
@@ -67,7 +67,93 @@ namespace DeliverIT.Models.DAL
                     CountryId = 4
                 }
             };
+
             db.Entity<City>().HasData(cities);
+
+            var addresses = new List<Address>()
+            {
+                new Address
+                {
+                    Id = 1,
+                    CityId = 1,
+                    StreetName = "Vasil Levski 14"
+                },
+                new Address
+                {
+                    Id = 2,
+                    CityId = 2,
+                    StreetName = "blv. Iztochen 23"
+                },
+                new Address
+                {
+                    Id = 3,
+                    CityId = 3,
+                    StreetName = "blv. Halic 12"
+                },
+                new Address
+                {
+                    Id = 4,
+                    CityId = 4,
+                    StreetName = "blv. Zeus 12"
+                },
+                new Address
+                {
+                    Id = 5,
+                    CityId = 5,
+                    StreetName = "blv. Romunska Morava 1"
+                }
+            };
+
+            db.Entity<Address>().HasData(addresses);
+
+            var statuses = new List<Status>()
+            {
+                new Status
+                {
+                    Id = 1,
+                    Name = "Preparing"
+                },
+                new Status
+                {
+                    Id = 2,
+                    Name = "On the way"
+                },
+                new Status
+                {
+                    Id = 3,
+                    Name = "Completed"
+                }
+            };
+
+            db.Entity<Status>().HasData(statuses);
+
+            var categories = new List<Category>()
+            {
+                new Category
+                {
+                    Id = 1,
+                    Name = "Electronics"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Shoes"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Clothing"
+                },
+                new Category
+                {
+                    Id = 1,
+                    Name = "Medical supplies"
+                }
+            };
+
+            db.Entity<Category>().HasData(categories);
+
+
         }
     }
 }
