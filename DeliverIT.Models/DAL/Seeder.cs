0b8e82632f32f7eaf7ca743f140a41363ab76c69
balimka <kbalimezov@gmail.com>
2021-10-01 14:24:28 +0300

    Completed Seed

--- DeliverIT.Models/DAL/Seeder.cs
+++ DeliverIT.Models/DAL/Seeder.cs
@@ -146,14 +146,144 @@ namespace DeliverIT.Models.DAL
                 },
                 new Category
                 {
-                    Id = 1,
+                    Id = 4,
                     Name = "Medical supplies"
                 }
             };
 
             db.Entity<Category>().HasData(categories);
 
+            var customers = new List<Customer>() 
+            { 
+                new Customer
+                {
+                    Id = 1,
+                    FirstName = "Misho",
+                    LastName = "Mishkov",
+                    Email = "mishkov@misho.com",
+                    AddressId = 1
+                },
+                new Customer
+                {
+                    Id = 2,
+                    FirstName = "Peter",
+                    LastName = "Petrov",
+                    Email = "petio@mvc.net",
+                    AddressId = 2
+                },
+                new Customer
+                {
+                    Id = 3,
+                    FirstName = "Koksal",
+                    LastName = "Baba",
+                    Email = "koksal@asd.tr",
+                    AddressId = 3
+                },
+                new Customer
+                {
+                    Id = 4,
+                    FirstName = "Nikolaos",
+                    LastName = "Tsitsibaris",
+                    Email = "indebt@greece.gov",
+                    AddressId = 4
+                }
+            };
 
+            db.Entity<Customer>().HasData(customers);
+
+            var employees = new List<Employee>()
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Djoro",
+                    LastName = "Emploev",
+                    Email = "djoro@ekont.com",
+                    AddressId = 1
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Speedy",
+                    LastName = "Gonzales",
+                    Email = "gonzales@speedy.net",
+                    AddressId = null
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "Dormut",
+                    LastName = "Baba",
+                    Email = "dormut@dhl.tr",
+                    AddressId = null
+                },
+                new Employee
+                {
+                    Id = 4,
+                    FirstName = "Stafanakis",
+                    LastName = "Kurierakis",
+                    Email = "ontime@fedex.us",
+                    AddressId = null
+                }
+            };
+
+            db.Entity<Employee>().HasData(employees);
+
+            var warehouses = new List<WareHouse>() 
+            {
+                new WareHouse
+                {
+                    Id = 1,
+                    AddressId = 1
+                },
+                new WareHouse
+                {
+                    Id = 2,
+                    AddressId = 2
+                }
+            };
+
+            db.Entity<WareHouse>().HasData(warehouses);
+
+            var shipments = new List<Shipment>()
+            {
+                new Shipment
+                {
+                    Id = 1,
+                    DepartureDate = System.DateTime.Today.AddDays(5),
+                    ArrivalDate = System.DateTime.Today.AddDays(10),
+                    OriginWareHouseId = 1,
+                    DestinationWareHouseId = 2,
+                    StatusId = 1
+                },
+                new Shipment
+                {
+                    Id = 2,
+                    DepartureDate = System.DateTime.Today.AddDays(5),
+                    ArrivalDate = System.DateTime.Today.AddDays(10),
+                    OriginWareHouseId = 1,
+                    DestinationWareHouseId = 2,
+                    StatusId = 1
+                }
+            };
+
+            db.Entity<Shipment>().HasData(shipments);
+
+            var parcels = new List<Parcel>()
+             {
+                new Parcel
+                {
+                    Id = 1,
+                    CustomerId = 1,
+                    ShipmentId = 1,
+                    WareHouseId = 1,
+                    CategoryId = 1,
+                    Weight = 1234.56,
+                    DeliverToAddress = true
+                }
+             };
+
+            db.Entity<Parcel>().HasData(parcels);
         }
     }
 }
