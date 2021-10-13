ffaa2604466aec463b821bf99f72766295c7cc77
Kalin Balimezov <kbalimezov@gmail.com>
2021-10-13 12:09:05 +0300

    Bug fixes -> FindUserService

--- DeliverIT.Services/Contracts/IAppAuthenticationService.cs
+++ DeliverIT.Services/Contracts/IAppAuthenticationService.cs
@@ -3,12 +3,8 @@ using System.Threading.Tasks;
 
 namespace DeliverIT.Services.Contracts
 {
-    public interface IAppAuthenticationService
+    public interface IFindUserService
     {
-        bool FindUser(string email);
-        bool FindEmployee(string email);
-        Task<string> GetUserID(string credentials);
-        Task<string> GetEmployeeID(string credentials);
         Task<UserDTO> FindUs(string authorization);
     }
 }
