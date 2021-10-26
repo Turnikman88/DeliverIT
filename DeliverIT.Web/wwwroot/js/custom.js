e2541e8b2e87e52767c5d03c9a3567bb3dfb449a
Georgi Petrov <georgi.petrov1820@abv.bg>
2021-10-27 00:19:17 +0300

    change view

--- DeliverIT.Web/wwwroot/js/custom.js
+++ DeliverIT.Web/wwwroot/js/custom.js
@@ -1,151 +1,94 @@
-(function ($) {
-	
-	"use strict";
+$(function () {
+    $("#tabs").tabs();
 
-	// Header Type = Fixed
-  $(window).scroll(function() {
+    if ($('.owl-trusted').length) {
+        $('.owl-trusted').owlCarousel({
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 4,
+            margin: 30,
+            autoplay: false,
+            smartSpeed: 700,
+            autoplayTimeout: 6000,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+                460: {
+                    items: 2,
+                    margin: 0
+                },
+                576: {
+                    items: 3,
+                    margin: 20
+                },
+                992: {
+                    items: 4,
+                    margin: 30
+                }
+            }
+        });
+    }
+    if ($('.owl-testimonials').length) {
+        $('.owl-testimonials').owlCarousel({
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            margin: 30,
+            autoplay: false,
+            smartSpeed: 700,
+            autoplayTimeout: 6000,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+                460: {
+                    items: 1,
+                    margin: 0
+                },
+                576: {
+                    items: 1,
+                    margin: 20
+                },
+                992: {
+                    items: 1,
+                    margin: 30
+                }
+            }
+        });
+    }
+});
+
+// Page loading animation
+$(window).on('load', function () {
+    if ($('.cover').length) {
+        $('.cover').parallax({
+            imageSrc: $('.cover').data('image'),
+            zIndex: '1'
+        });
+    }
+
+    $("#preloader").animate({
+        'opacity': '0'
+    }, 600, function () {
+        setTimeout(function () {
+            $("#preloader").css("visibility", "hidden").fadeOut();
+        }, 300);
+    });
+});
+
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
     var box = $('.header-text').height();
     var header = $('header').height();
 
     if (scroll >= box - header) {
-      $("header").addClass("background-header");
+        $("header").addClass("background-header");
     } else {
-      $("header").removeClass("background-header");
+        $("header").removeClass("background-header");
     }
-  });
-
-
-	$('.loop').owlCarousel({
-      center: true,
-      items:1,
-      loop:true,
-      autoplay: true,
-      nav: true,
-      margin:0,
-      responsive:{ 
-          1200:{
-              items:5
-          },
-          992:{
-              items:3
-          },
-          760:{
-            items:2
-        }
-      }
-  });
-	
-
-	// Menu Dropdown Toggle
-  if($('.menu-trigger').length){
-    $(".menu-trigger").on('click', function() { 
-      $(this).toggleClass('active');
-      $('.header-area .nav').slideToggle(200);
-    });
-  }
-
-
-  // Menu elevator animation
-  $('.scroll-to-section a[href*=\\#]:not([href=\\#])').on('click', function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        var width = $(window).width();
-        if(width < 991) {
-          $('.menu-trigger').removeClass('active');
-          $('.header-area .nav').slideUp(200);  
-        }       
-        $('html,body').animate({
-          scrollTop: (target.offset().top) + 1
-        }, 700);
-        return false;
-      }
-    }
-  });
-
-  $(document).ready(function () {
-      $(document).on("scroll", onScroll);
-      
-      //smoothscroll
-      $('.scroll-to-section a[href^="#"]').on('click', function (e) {
-          e.preventDefault();
-          $(document).off("scroll");
-          
-          $('.scroll-to-section a').each(function () {
-              $(this).removeClass('active');
-          })
-          $(this).addClass('active');
-        
-          var target = this.hash,
-          menu = target;
-          var target = $(this.hash);
-          $('html, body').stop().animate({
-              scrollTop: (target.offset().top) + 1
-          }, 500, 'swing', function () {
-              window.location.hash = target;
-              $(document).on("scroll", onScroll);
-          });
-      });
-  });
-
-  function onScroll(event){
-      var scrollPos = $(document).scrollTop();
-      $('.nav a').each(function () {
-          var currLink = $(this);
-          var refElement = $(currLink.attr("href"));
-          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-              $('.nav ul li a').removeClass("active");
-              currLink.addClass("active");
-          }
-          else{
-              currLink.removeClass("active");
-          }
-      });
-  }
-
-
-  // Acc
-  $(document).on("click", ".naccs .menu div", function() {
-    var numberIndex = $(this).index();
-
-    if (!$(this).is("active")) {
-        $(".naccs .menu div").removeClass("active");
-        $(".naccs ul li").removeClass("active");
-
-        $(this).addClass("active");
-        $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
-
-        var listItemHeight = $(".naccs ul")
-          .find("li:eq(" + numberIndex + ")")
-          .innerHeight();
-        $(".naccs ul").height(listItemHeight + "px");
-      }
-  });
-
-
-	// Page loading animation
-	 $(window).on('load', function() {
-
-        $('#js-preloader').addClass('loaded');
-
-    });
-
-	
-
-	// Window Resize Mobile Menu Fix
-  function mobileNav() {
-    var width = $(window).width();
-    $('.submenu').on('click', function() {
-      if(width < 767) {
-        $('.submenu ul').removeClass('active');
-        $(this).find('ul').toggleClass('active');
-      }
-    });
-  }
-
-
-
-
-})(window.jQuery);
+});
